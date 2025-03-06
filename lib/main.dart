c8ea9f2fc866b192bb04b26c3e741027aad60a1e
Jayasri-1606 <kjayasri1606@gmail.com>
2025-03-06 12:17:35 +0530

    update app logo

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fruit Hub Delivery App',
+      title: 'Fruit Hub',
       home: SplashPage(),
     );
   }
