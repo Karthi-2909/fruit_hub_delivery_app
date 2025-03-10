d1575f9b32d14fac084b0f63d8ddc3e2ce6bfc40
Karthi-2909 <karthikraguraman29@gmail.com>
2025-03-10 15:10:07 +0530

    page updated

--- lib/login/aunthetication_page.dart
+++ lib/login/aunthetication_page.dart
@@ -10,10 +10,9 @@ class AuntheticationPage extends StatefulWidget {
 }
 
 class _AuntheticationPageState extends State<AuntheticationPage> {
-  final TextEditingController _nameController =
-      TextEditingController(); // Controller for text field
-
+  final TextEditingController _nameController = TextEditingController();
   String? _errorText;
+
   void _validateAndProceed() {
     setState(() {
       if (_nameController.text.isEmpty) {
@@ -32,132 +31,128 @@ class _AuntheticationPageState extends State<AuntheticationPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: true,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.6,
-              width: double.infinity,
-              color: AppColors.primarycolor,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 200),
-                        child: Image.asset(
-                          'assets/images/fruit_drop.png',
-                          width: 40,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                children: [
+                  // Top Section with Image
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    width: double.infinity,
+                    color: AppColors.primarycolor,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 80),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 200),
+                              child: Image.asset(
+                                'assets/images/fruit_drop.png',
+                                width: 40,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 18),
+                              child: Image.asset(
+                                'assets/images/fruit_basket.png',
+                                height: 260,
+                                width: 330,
+                              ),
+                            ),
+                            const SizedBox(height: 3),
+                            Image.asset(
+                              'assets/images/fruit_basket_shadow.png',
+                              width: 260,
+                            ),
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 18),
-                        child: Image.asset(
-                          'assets/images/fruit_basket.png',
-                          height: 260,
-                          width: 330,
-                        ),
-                      ),
-                      SizedBox(height: 3),
-                      Image.asset(
-                        'assets/images/fruit_basket_shadow.png',
-                        width: 260,
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-        
-            // Secondary Theme Container with TextBox
-            Expanded(
-              // Expands to fit remaining space
-              flex: 4, // 40% of the screen
-              child: Container(
-                color: AppColors.secondarycolor,
-                width: double.infinity,
-                padding: const EdgeInsets.all(20),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment:
-                        MainAxisAlignment.start, // Centers content vertically
-                    children: [
-                      // Question
-                      Text(
-                        "What is your firstname?",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 82, 79, 79),
+
+                  // Bottom Section with TextField and Button
+                  Container(
+                    color: AppColors.secondarycolor,
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Question
+                        Text(
+                          "What is your firstname?",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 82, 79, 79),
+                        ),),
+
+                      SizedBox(height: 10),
+
+                        // TextField
+                        TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            hintText: "Tony",
+                            hintStyle: TextStyle(color: Colors.grey.shade500),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            errorText: _errorText,
+                          ),
                         ),
-                      ),
-        
-                      SizedBox(height: 10), // Space between text and TextBox
-        
-                      // TextField (Input Box)
-                      TextField(
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                          hintText: "Tony",
-                          hintStyle: TextStyle(color: Colors.grey.shade500),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Slightly rounded corners
-                            borderSide: BorderSide.none, // Removes the border
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Ensure consistent rounding
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide.none,
-                          ),
-                          filled: true,
-                          fillColor: Colors.grey.shade200,
-                          contentPadding:
-                              EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                              errorText: _errorText,
-                        ),
-                      ),
-        
-                      SizedBox(
-                        height: 40,
-                      ),
-                      SizedBox(
-                        height: 50,
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: _validateAndProceed,
-                          style: ElevatedButton.styleFrom(
+
+                        const SizedBox(height: 40),
+
+                        // Start Ordering Button
+                        SizedBox(
+                          height: 50,
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: _validateAndProceed,
+                            style: ElevatedButton.styleFrom(
                               backgroundColor: AppColors.primarycolor,
                               foregroundColor: AppColors.secondarycolor,
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10),
-                              )),
-                          child: Text(
-                            "Start Ordering",
-                            style: TextStyle(
-                                fontSize: 13, fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                            child: Text(
+                              "Start Ordering",
+                              style: TextStyle(
+                                  fontSize: 13, fontWeight: FontWeight.w600),
+                            ),
                           ),
                         ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
-          ],
-             ),
+          );
+        },
       ),
-);}
+    );
+  }
 }
